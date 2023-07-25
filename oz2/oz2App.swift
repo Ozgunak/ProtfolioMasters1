669896a983f03ac374f158102061a2e4ae0ca673
Özgün Aksoy <zgnaksoy@gmail.com>
2023-07-25 16:17:47 -0700

    Initial Commit

---
+++ oz2/oz2App.swift
@@ -0,0 +1,17 @@
+//
+//  oz2App.swift
+//  oz2
+//
+//  Created by özgün aksoy on 2023-07-25.
+//
+
+import SwiftUI
+
+@main
+struct oz2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
